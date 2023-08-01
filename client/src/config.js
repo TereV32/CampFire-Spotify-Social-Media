75b59edf18159814a604361f7b8de2ccd25847b0
tvill08 <63677685+TereV32@users.noreply.github.com>
2023-08-01 13:43:56 -0500

    organized files and made changes

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ let baseUrl = '';
 
 if (process.env.NODE_ENV === 'production') {
   // Set the base URL to localhost during development
-  baseUrl = `https://campfire-1f3fc409de5e.herokuapp.com/${process.env.PORT}`;
+  baseUrl = 'https://campfire-1f3fc409de5e.herokuapp.com';
 } else {
   // Set the base URL to the actual production URL
   baseUrl = 'http://localhost:3001';
